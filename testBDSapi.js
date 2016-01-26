13a9dcc8f32765130addc4dfa2106b1b0d393102
Pavel Zhuravlev <Pzhuravlev@econometricainc.com>
2016-01-26 18:30:30 -0500

    Finished separation of Model for now. Added observables for other fields multiple selections

--- testBDSapi.js
+++ testBDSapi.js
@@ -1,189 +1,46 @@
-var BDSVisModel = {
-	states : [
-		//{"code" : "00", "name" : "United States", "st":"US"},
-		{"code" : "01", "name" : "Alabama", "st" : "AL" },
-		{"code" : "02", "name" : "Alaska", "st" : "AK" },
-		{"code" : "04", "name" : "Arizona", "st" : "AZ" },
-		{"code" : "05", "name" : "Arkansas", "st" : "AR" },
-		{"code" : "06", "name" : "California", "st" : "CA" },
-		{"code" : "08", "name" : "Colorado", "st" : "CO" },
-		{"code" : "09", "name" : "Connecticut", "st" : "CT" },
-		{"code" : "10", "name" : "Delaware", "st" : "DE" },
-		{"code" : "11", "name" : "District of Columbia", "st" : "DC" },
-		{"code" : "12", "name" : "Florida", "st" : "FL" },
-		{"code" : "13", "name" : "Georgia", "st" : "GA" },
-		{"code" : "15", "name" : "Hawaii", "st" : "HI" },
-		{"code" : "16", "name" : "Idaho", "st" : "ID" },
-		{"code" : "17", "name" : "Illinois", "st" : "IL" },
-		{"code" : "18", "name" : "Indiana", "st" : "IN" },
-		{"code" : "19", "name" : "Iowa", "st" : "IA" },
-		{"code" : "20", "name" : "Kansas", "st" : "KS" },
-		{"code" : "21", "name" : "Kentucky", "st" : "KY" },
-		{"code" : "22", "name" : "Louisiana", "st" : "LA" },
-		{"code" : "23", "name" : "Maine", "st" : "ME" },
-		{"code" : "24", "name" : "Maryland", "st" : "MD" },
-		{"code" : "25", "name" : "Massachusetts", "st" : "MA" },
-		{"code" : "26", "name" : "Michigan", "st" : "MI" },
-		{"code" : "27", "name" : "Minnesota", "st" : "MN" },
-		{"code" : "28", "name" : "Mississippi", "st" : "MS" },
-		{"code" : "29", "name" : "Missouri", "st" : "MO" },
-		{"code" : "30", "name" : "Montana", "st" : "MT" },
-		{"code" : "31", "name" : "Nebraska", "st" : "NE" },
-		{"code" : "32", "name" : "Nevada", "st" : "NV" },
-		{"code" : "33", "name" : "New Hampshire", "st" : "NH" },
-		{"code" : "34", "name" : "New Jersey", "st" : "NJ" },
-		{"code" : "35", "name" : "New Mexico", "st" : "NM" },
-		{"code" : "36", "name" : "New York", "st" : "NY" },
-		{"code" : "37", "name" : "North Carolina", "st" : "NC" },
-		{"code" : "38", "name" : "North Dakota", "st" : "ND" },
-		{"code" : "39", "name" : "Ohio", "st" : "OH" },
-		{"code" : "40", "name" : "Oklahoma", "st" : "OK" },
-		{"code" : "41", "name" : "Oregon", "st" : "OR" },
-		{"code" : "42", "name" : "Pennsylvania", "st" : "PA" },
-		{"code" : "44", "name" : "Rhode Island", "st" : "RI" },
-		{"code" : "45", "name" : "South Carolina", "st" : "SC" },
-		{"code" : "46", "name" : "South Dakota", "st" : "SD" },
-		{"code" : "47", "name" : "Tennessee", "st" : "TN" },
-		{"code" : "48", "name" : "Texas", "st" : "TX" },
-		{"code" : "49", "name" : "Utah", "st" : "UT" },
-		{"code" : "50", "name" : "Vermont", "st" : "VT" },
-		{"code" : "51", "name" : "Virginia", "st" : "VA" },
-		{"code" : "53", "name" : "Washington", "st" : "WA" },
-		{"code" : "54", "name" : "West Virginia", "st" : "WV" },
-		{"code" : "55", "name" : "Wisconsin", "st" : "WI" },
-		{"code" : "56", "name" : "Wyoming", "st" : "WY" }],
-	fage : [
-		{"code" : "a", "name" : "0" },
-		{"code" : "b", "name" : "1" },
-		{"code" : "c", "name" : "2" },
-		{"code" : "d", "name" : "3" },
-		{"code" : "e", "name" : "4" },
-		{"code" : "f", "name" : "5" },
-		{"code" : "g", "name" : "6-10" },
-		{"code" : "h", "name" : "11-15" },
-		{"code" : "i", "name" : "16-20" },
-		{"code" : "j", "name" : "21-25" },
-		{"code" : "k", "name" : "26+" },
-		{"code" : "l", "name" : "Born before '76" },
-		{"code" : "m", "name" : "All Ages" }],
-
-	fsize : [
-		{"code" : "a", "name" : "1-4" },
-		{"code" : "b", "name" : "5-9" },
-		{"code" : "c", "name" : "10-19" },
-		{"code" : "d", "name" : "20-49" },
-		{"code" : "e", "name" : "50-99" },
-		{"code" : "f", "name" : "100-249" },
-		{"code" : "g", "name" : "250-499" },
-		{"code" : "h", "name" : "500-999" },
-		{"code" : "i", "name" : "1000-2499" },
-		{"code" : "j", "name" : "2500-4999" },
-		{"code" : "k", "name" : "5000-9999" },
-		{"code" : "l", "name" : "10000+" },
-		{"code" : "m", "name" : "All Sizes" }],
-
-	sectors : [
-		{"code" : 00, "acr" : "EW", "name" : "Economy Wide" },
-		{"code" : 07, "acr" : "AGR", "name" : "Agriculture, Forestry, and Fishing" },
-		{"code" : 10, "acr" : "MIN", "name" : "Mining" },
-		{"code" : 15, "acr" : "CON", "name" : "Construction" },
-		{"code" : 20, "acr" : "MAN", "name" : "Manufacturing" },
-		{"code" : 40, "acr" : "TCU", "name" : "Transportation, Communication, and Public Utilities" },
-		{"code" : 50, "acr" : "WHO", "name" : "Wholesale Trade" },
-		{"code" : 52, "acr" : "RET", "name" : "Retail Trade" },
-		{"code" : 60, "acr" : "FIRE", "name" : "Finance, Insurance, and Real Estate" },
-		{"code" : 70, "acr" : "SRV", "name" : "Services" }],
-		
-	measures : [
-		{"code" : "firms", "name" : "Number of firms" },
-		{"code" : "estabs", "name" : "Number of establishments" },
-		{"code" : "emp", "name" : "Employment" },
-		{"code" : "estabs_entry", "name" : "Establishments born during the last 12 months" },
-		{"code" : "estabs_entry_rate", "name" : "Establishment birth rate" },
-		{"code" : "estabs_exit", "name" : "Establishments exiting during the last 12 months" },
-		{"code" : "estabs_exit_rate", "name" : "Establishment exit rate" },
-		{"code" : "job_creation", "name" : "Jobs created over the last 12 months" },
-		{"code" : "job_creation_births", "name" : "Jobs created by establishment births over the last 12 months" },
-		{"code" : "job_creation_continuers", "name" : "Jobs created by continuing establishments over the last 12 months" },
-		{"code" : "job_creation_rate_births", "name" : "Jobs creation rate from establishment births" },
-		{"code" : "job_creation_rate", "name" : "Job creation rate" },
-		{"code" : "job_destruction", "name" : "Jobs destroyed within the last 12 months" },
-		{"code" : "job_destruction_deaths", "name" : "Jobs destroyed by establishment exit over the last 12 months" },
-		{"code" : "job_destruction_continuer", "name" : "Jobs destroyed at continuing establishments over the last 12 months" },
-		{"code" : "job_destruction_rate_deat", "name" : "Jobs destruction rate from establishment exit" },
-		{"code" : "job_destruction_rate", "name" : "Jobs destruction rate" },
-		{"code" : "net_job_creation", "name" : "Net job creation" },
-		{"code" : "net_job_creation_rate", "name" : "Net job creation rate" },
-		{"code" : "reallocation_rate", "name" : "Reallocation rate" },
-		{"code" : "firmdeath_firms", "name" : "Number of firm exits" },
-		{"code" : "firmdeath_estabs", "name" : "Establishment exit due to firm death" },
-		{"code" : "firmdeath_emp", "name" : "Job destruction from firm exit" }],
-
-	InitModel : function() {
-		
-	}
-
-};
-
-
-
 var ViewModel = function() {
 	var self = this;
 
 	this.model = BDSVisModel;
-
-	this.measlookup={}; this.statelookup={};this.fagelookup={};this.fsizelookup={};
+	this.model.InitModel();
+	
 
 	this.data = ko.observableArray();
 	this.data2show = ko.observableArray();
 
 	this.ShowData = ko.observable(0);
 
-	self.toggleshowdata = function () {
+	this.toggleshowdata = function () {
 		if (self.ShowData()) {self.ShowData(0)} else (self.ShowData(1));
 	}
 
 	this.sector = ko.observable(this.model.sectors[0].code);
 	this.measure = ko.observable(this.model.measures[18].code);
-	this.state = ko.observable(this.model.states[0].code);
-	this.SelectedStates = ko.observableArray([this.model.states[02].code]);
+	this.state = ko.observable(this.model.states[6].code);
+	this.SelectedStates = ko.observableArray([this.state()]);
+
+	this.StateAsLegend = ko.observable(true);
+	this.MeasureAsLegend = ko.observable(false);
+	this.SectorAsLegend = ko.observable(false);
+	this.YearAsLegend = ko.observable(false);
+	this.FirmCharAsLegend = ko.observable(false);
 
 	//Subscribe to input changes
 	self.sector.subscribe( function(newValue) {
-		getBDSdata(newValue,self.measure(), self.SelectedStates());
+		self.getBDSdata(newValue,self.measure(), self.SelectedStates());
 	})
 
 	self.measure.subscribe( function(newValue) {
-		getBDSdata(self.sector(),newValue,self.SelectedStates());
+		self.getBDSdata(self.sector(),newValue,self.SelectedStates());
 	})
 
 	self.SelectedStates.subscribe( function(newValue) {
-		getBDSdata(self.sector(),self.measure(),newValue);
+		self.getBDSdata(self.sector(),self.measure(),newValue);
 	}) 
 
-	//Initial request and plot
-	getBDSdata(self.sector(),self.measure(),self.SelectedStates());
 	
-
-	var InitVM = function () {
-
-		//Create dictionaries/hashmaps to lookup names of categorical variable values
-		for (var i in self.model.states)
-			self.statelookup[self.model.states[i].code]=self.model.states[i].name;
-		for (var i in self.model.measures)
-			self.measlookup[self.model.measures[i].code]=self.model.measures[i].name;
-		for (var i in self.model.fage)
-			self.fagelookup[self.model.fage[i].code]=self.model.fage[i].name;
-		for (var i in self.model.fsize)
-			self.fsizelookup[self.model.fsize[i].code]=self.model.fsize[i].name;
-
-		
-	}
-
-	InitVM();
-
-
-	function getBDSdata(sic1,measure,states) {
+//Get the data from the API according to current request and render it into array of objects with field names corresponding to the variables
+	this.getBDSdata = function (sic1,measure,states) {
 
 		var requestdata={
     		//"get": ["sic1","job_creation_rate","fage4"],
@@ -215,30 +72,33 @@ var ViewModel = function() {
 	    			jsoned.push(rec);
 	    		}
 	    	}
-	    	updateBDSdata(jsoned,measure,states);
+	    	self.updateBDSdata(jsoned,measure,states);
 	    });
 	}
 
-	function updateBDSdata(data,measure,states) {
+//Process data obtained from API. Change codes into names, add state list number (istate), form data2show for displaying as a table and call the function making the plot
+	this.updateBDSdata = function(data,measure,states) {
 
 		var data2show=[];
 
 		for (var i in data) {
-			data[i].fage4=self.fagelookup[data[i].fage4];
+			data[i].fage4=self.model.fagelookup[data[i].fage4];
 			for (var j in states) {
 				if (states[j]==data[i]['state'])
 					data[i]['istate']=j
 			}
-			data[i].state=self.statelookup[data[i].state];
+			data[i].state=self.model.statelookup[data[i].state];
 			data[i].value=data[i][measure];
 		}
 
 		self.data(data);
 
-		makeBarChart(self.data(),measure,states);
+		self.makeBarChart(self.data(),measure,states);
 	}
 
-	function makeBarChart(data,measure,states) {
+	
+
+	this.makeBarChart = function (data,measure,states) {
 		var margin = {top: 20, right: 30, bottom: 30, left: 80},
 		width = 960 - margin.left - margin.right,
 		height = 500 - margin.top - margin.bottom;
@@ -343,10 +203,11 @@ var ViewModel = function() {
 			.append("text")
 			.attr("fill","black")
 			.attr("x",(symbolsize+5)).attr("y",function(d,i) {return 15+(symbolsize+5)*i;})
-			.text(function(d) { return self.statelookup[d];});
+			.text(function(d) { return self.model.statelookup[d];});
 	}
+
+//Initial request and plot
+	this.getBDSdata(self.sector(),self.measure(),self.SelectedStates());
 }
-
-
 
 ko.applyBindings(new ViewModel());
