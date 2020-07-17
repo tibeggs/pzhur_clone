fa0dc1b7fd5b20ec4966b71f41688607f2cf4a8e
tibeggs <tibeggs@gmail.com>
2020-07-17 14:47:51 -0400

    as

--- js/PlotView.js
+++ js/PlotView.js
@@ -157,7 +157,7 @@ BDSVis.PlotView = {
 			};
 
 			//X-axis variable selector			
-			var selector = this.xvarselector.appendChild("button");
+			var selector = this.xvarselector.appendChild("select");
 			AddOptionsToVarSelector(selector,vm.model.variables.filter(function(d){return (d.asaxis && d.code!==vm.cvar)}),"xvar",false);
 			selector.on("change", function() { vm.setxvar(this.value);} );
 			if (vm.model.IsGroup(vm.xvar)) {
